28ab033a80ec4bb20031b5735a53acab6b86545a
Prabjot Singh <125903944+singh1733@users.noreply.github.com>
2024-06-27 22:09:21 -0400

    Fixed bug where turn switched even if it was a miss

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import _ from "lodash";
 import "./style.css";
 import { Ship, GameBoard, Players } from "./classes";
 
+const players = new Players();
+
 function createGame() {
-  const players = new Players();
   populateBoard(players.real);
   populateBoard(players.computer);
   loadPhysicalBoard(players);
@@ -31,7 +32,7 @@ function loadPhysicalBoard(players) {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let square = document.createElement("button");
-      square.id = "" + i + "" + j;
+      square.id = "" + i + "" + j + "real";
       let boardSquare = players.real.board[i][j];
       if (boardSquare === "empty") {
         square.classList.add("empty");
@@ -42,14 +43,14 @@ function loadPhysicalBoard(players) {
       }
       realBoard.appendChild(square);
       square.addEventListener("click", () =>
-        processClick(square, players.real)
+        processClick(square, players.real, players.real)
       );
     }
   }
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let square = document.createElement("button");
-      square.id = "" + i + "" + j;
+      square.id = "" + i + "" + j + "computer";
       let boardSquare = players.computer.board[i][j];
       if (boardSquare === "empty" || boardSquare === "missed") {
         square.classList.add(boardSquare);
@@ -58,25 +59,33 @@ function loadPhysicalBoard(players) {
       }
       compBoard.appendChild(square);
       square.addEventListener("click", () =>
-        processClick(square, players.computer)
+        processClick(square, players.computer, players.real)
       );
     }
   }
 }
 
-function processClick(square, player) {
+function processClick(square, player, real) {
   let coordinateContent =
     player.board[square.id.charAt(0) - "0"][square.id.charAt(1) - "0"];
   if (coordinateContent === "missed") {
+    if (currentPlayer === "computer") {
+      setTimeout(() => computerTurn(player, real, real), 1000);
+    }
   } else if (coordinateContent === "empty") {
-    coordinateContent = "missed";
+    player.board[square.id.charAt(0) - "0"][square.id.charAt(1) - "0"] =
+      "missed";
     square.classList.add("miss");
-    square.innerHTML="<div></div>"
-    switchTurn();
+    square.classList.remove("empty");
+    square.innerHTML = "<div></div>";
+    switchTurn(player, real);
   } else {
     player.receiveAttack(square.id.charAt(0) - "0", square.id.charAt(1) - "0");
     square.classList.remove("ship");
     square.classList.add("hit");
+    if (currentPlayer === "computer") {
+      setTimeout(() => computerTurn(player, real, real), 1000);
+    }
   }
 
   if (player.reportAllSunk()) {
@@ -84,7 +93,7 @@ function processClick(square, player) {
   }
 }
 
-function switchTurn() {
+function switchTurn(player, real) {
   currentPlayer = currentPlayer === "real" ? "computer" : "real";
   if (currentPlayer === "computer") {
     realBoard.querySelectorAll("button").forEach((node) => {
@@ -95,6 +104,7 @@ function switchTurn() {
       node.disabled = true;
       node.style.opacity = ".5";
     });
+    setTimeout(() => computerTurn(player, real, real), 1000);
   } else {
     realBoard.querySelectorAll("button").forEach((node) => {
       node.disabled = true;
@@ -104,6 +114,21 @@ function switchTurn() {
       node.disabled = false;
       node.style.opacity = "1";
     });
+  }
+}
+
+function computerTurn(player, real) {
+  while (true) {
+    let row = Math.floor(Math.random() * 10);
+    let column = Math.floor(Math.random() * 10);
+    let square = document.getElementById("" + row + "" + column + "real");
+    if (
+      !square.classList.contains("missed") &&
+      !square.classList.contains("hit")
+    ) {
+      processClick(square, real, real);
+      return;
+    }
   }
 }
 
